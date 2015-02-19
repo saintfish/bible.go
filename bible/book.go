a60180affbac11c963ec2a4701af734ee6407510
Sheng Yu <yusheng.sjtu@gmail.com>
2015-02-19 11:50:41 -0800

    Add JSON support for BookID

--- bible/book.go
+++ bible/book.go
@@ -1,7 +1,14 @@
 package bible
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+//go:generate stringer --type BookID
+
 // BookID is a numerical identifier of a book in the bible.
-type BookID byte
+type BookID int
 
 // A list of books in the bible
 const (
@@ -89,80 +96,40 @@ const (
 // NumBooks is the number of the books in the bible
 const NumBooks = int(LastBook - FirstBook + 1)
 
-var bookNames = [...]string{
-	"", // invalid
-	"Genesis",
-	"Exodus",
-	"Leviticus",
-	"Numbers",
-	"Deuteronomy",
-	"Joshua",
-	"Judges",
-	"Ruth",
-	"Samuel1",
-	"Samuel2",
-	"Kings1",
-	"Kings2",
-	"Chronicles1",
-	"Chronicles2",
-	"Ezra",
-	"Nehemiah",
-	"Esther",
-	"Job",
-	"Psalm",
-	"Proverbs",
-	"Ecclesiastes",
-	"SongOfSongs",
-	"Isaiah",
-	"Jeremiah",
-	"Lamentations",
-	"Ezekiel",
-	"Daniel",
-	"Hosea",
-	"Joel",
-	"Amos",
-	"Obadiah",
-	"Jonah",
-	"Micah",
-	"Nahum",
-	"Habakkuk",
-	"Zephaniah",
-	"Haggai",
-	"Zechariah",
-	"Malachi",
-	"Matthew",
-	"Mark",
-	"Luke",
-	"John",
-	"Acts",
-	"Romans",
-	"Corinthians1",
-	"Corinthians2",
-	"Galatians",
-	"Ephesians",
-	"Philippians",
-	"Colossians",
-	"Thessalonians1",
-	"Thessalonians2",
-	"Timothy1",
-	"Timothy2",
-	"Titus",
-	"Philemon",
-	"Hebrews",
-	"James",
-	"Peter1",
-	"Peter2",
-	"John1",
-	"John2",
-	"John3",
-	"Jude",
-	"Revelation",
+var nameBookIDMap map[string]BookID
+
+func init() {
+	nameBookIDMap = make(map[string]BookID)
+	for b := FirstBook; b <= LastBook; b++ {
+		nameBookIDMap[b.String()] = b
+	}
 }
 
-// BookName gives the string representation of BookID enum.
-func BookName(b BookID) string {
-	if b >= FirstBook && b <= LastBook {
-		return bookNames[b]
+func BookIDFromName(name string) BookID {
+	b, ok := nameBookIDMap[name]
+	if !ok {
+		return InvalidBook
 	}
-	return bookNames[InvalidBook]
+	return b
+}
+
+func (b BookID) MarshalJSON() ([]byte, error) {
+	if b >= FirstBook && b <= LastBook {
+		return json.Marshal(b.String())
+	}
+	return nil, fmt.Errorf("Invalid BookID %s", b)
+}
+
+func (b *BookID) UnmarshalJSON(data []byte) error {
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+	book := BookIDFromName(s)
+	if book == InvalidBook {
+		return fmt.Errorf("Invalid book name %s", s)
+	}
+	*b = book
+	return nil
 }
--- bible/book_test.go
+++ bible/book_test.go
@@ -1,6 +1,8 @@
 package bible
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 )
 
@@ -16,14 +18,67 @@ func TestBookName(t *testing.T) {
 		{id: FirstNT, name: "Matthew"},
 		{id: LastNT, name: "Revelation"},
 		{id: 1, name: "Genesis"},
-		{id: InvalidBook, name: ""},
-		{id: 100, name: ""},
+		{id: InvalidBook, name: "InvalidBook"},
+		{id: 100, name: "BookID(100)"},
 	}
 	for _, data := range bookNameData {
-		if BookName(data.id) != data.name {
+		if data.id.String() != data.name {
 			t.Errorf(
 				"Name of BookID %d is %s, expected %s",
-				data.id, BookName(data.id), data.name)
+				data.id, data.id, data.name)
+		}
+	}
+}
+
+type bookWrapper struct {
+	Book BookID
+}
+
+func TestJSON(t *testing.T) {
+	for b := FirstBook; b <= LastBook; b++ {
+		source := bookWrapper{b}
+		dest := bookWrapper{}
+		data, err := json.Marshal(source)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		err = json.Unmarshal(data, &dest)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if source.Book != dest.Book {
+			t.Errorf(
+				"BookID changed after transfering to JSON: %v v.s. %v",
+				source, dest)
+		}
+	}
+}
+
+func TestInvalidMarshalJSON(t *testing.T) {
+	invalidBookID := []BookID{InvalidBook, -1, 100}
+	for _, b := range invalidBookID {
+		wrapper := bookWrapper{b}
+		data, err := json.Marshal(wrapper)
+		if err == nil {
+			t.Errorf(
+				"Invalid BookID %d is unexpected to be marshaled as %s",
+				b, data)
+		}
+	}
+}
+
+func TestInvalidUnmarshalJSON(t *testing.T) {
+	invalidBookName := []string{"InvalidBook", "", "BookID(10)"}
+	for _, name := range invalidBookName {
+		data := []byte(fmt.Sprintf(`{ "Book": "%s" }`, name))
+		wrapper := bookWrapper{}
+		err := json.Unmarshal(data, &wrapper)
+		if err == nil {
+			t.Errorf(
+				"Invalid book name %s is unexpected to be unmarshaled as %s",
+				name, wrapper)
 		}
 	}
 }
